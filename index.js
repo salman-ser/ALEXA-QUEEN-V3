4a6f8655eb4d4dd701726261f06b151a4d0c6a88
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-08-09 21:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("./config.js")
 const { default: AlexaConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
-const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
+const { state, saveState } = useSingleFileAuthState(`./session.json`)
 const pino = require('pino')
 const fs = require('fs')
 const chalk = require('chalk')
