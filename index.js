47cdd9fe120de0e6a9abc5965da04e0d154836b3
TOXIC KICHU✨ <97310543+TOXIC-KICHU@users.noreply.github.com>
2022-08-09 15:36:42 +0530

    Fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("./config.js")
-require ("./alexa.js")
 const { default: AlexaConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
@@ -71,7 +70,7 @@ if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 if (!Alexa.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
 if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
 m = smsg(Alexa, mek, store)
-require("./liza.js")(Alexa, m, chatUpdate, store)
+require("./alexa.js")(Alexa, m, chatUpdate, store)
 } catch (err) {
 console.log(err)
 }
